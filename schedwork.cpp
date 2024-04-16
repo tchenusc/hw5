70b23cad34f78d941d80348cd94a1b366c388a45
Tony Chen <tchen776@usc.edu>
2024-04-15 17:53:55 -0700

    Project complete

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool assignDaily(const AvailabilityMatrix& avail,
+                 const size_t dailyNeed,
+                 const size_t maxShifts,
+                 DailySchedule& sched, int n, int d, vector<int>& numShiftsScheduled);
+bool checkValidity(const size_t maxShifts, DailySchedule& sched, size_t day, vector<int>& numShiftsScheduled, Worker_T id);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +41,63 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
+    
+    size_t numWorkers = avail[0].size();
+    if (numWorkers < dailyNeed) return false;
+    
+    vector<int> numShiftsScheduled(numWorkers);
+    fill(numShiftsScheduled.begin(), numShiftsScheduled.end(), 0);
+    
+    for (size_t i = 0; i < avail.size(); i++)
+    {
+        vector<Worker_T> dailySched;
+        for (size_t i = 0; i < dailyNeed; i++)
+        {
+            dailySched.push_back(INVALID_ID);
+        }
+        sched.push_back(dailySched);
+    }
+    
+    
+    bool status = assignDaily(avail, dailyNeed, maxShifts, sched, 0, 0, numShiftsScheduled);
+    return status;
 
 }
 
+bool assignDaily(const AvailabilityMatrix& avail,
+                 const size_t dailyNeed,
+                 const size_t maxShifts,
+                 DailySchedule& sched, int n, int d, vector<int>& numShiftsScheduled)
+{
+    if ((unsigned int)n == avail.size()-1 && (unsigned int)d == dailyNeed)
+    {
+        return true;
+    }
+    
+    if ((unsigned int)d >= dailyNeed)
+    {
+        return assignDaily(avail, dailyNeed, maxShifts, sched, n+1, 0, numShiftsScheduled);
+    }
+    
+    
+    for (size_t i = 0; i < avail[0].size(); i++)
+    {
+        sched[n][d] = i;
+        numShiftsScheduled[i]++;
+        if (avail[n][i] == 1 && checkValidity(maxShifts, sched, n, numShiftsScheduled, i)) // is available
+        {
+            bool status = assignDaily(avail, dailyNeed, maxShifts, sched, n, d+1, numShiftsScheduled);
+            if (status) return true;
+        }
+        numShiftsScheduled[i]--;
+    }
+    sched[n][d] = INVALID_ID;
+    return false;
+}
+
+bool checkValidity(const size_t maxShifts, DailySchedule& sched, size_t day, vector<int>& numShiftsScheduled, Worker_T id)
+{
+    if (count(sched[day].begin(), sched[day].end(), id) != 1) return false;
+    if ((unsigned int)numShiftsScheduled[id] > maxShifts) return false;
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,6 +14,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void computeKnownCharacters(string in, string floating, set<string>& ret, const set<string>& dict, set<string>& knownCharacters);
+void computeUnknownCharacters(string in, set<string>& out, size_t indexToInsert, const set<string>& dict);
 
 
 // Definition of primary wordle function
@@ -22,7 +25,55 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> ret;
+    set<string> knownCharacters;
+    computeKnownCharacters(in, floating, ret, dict, knownCharacters);
+    
+    set<string>::iterator it;
+    for (it = knownCharacters.begin(); it != knownCharacters.end(); ++it)
+    {
+        computeUnknownCharacters(*it, ret, 0, dict);
+    }
+    
+    return ret;
 }
 
-// Define any helper functions here
+void computeKnownCharacters(string in, string floating, set<string>& ret, const set<string>& dict, set<string>& knownCharacters)
+{
+    if (floating.size() == 0)
+    {
+        //cout << in << endl; //Testing purposes
+        //computeUnknownCharacters(in, ret, 0, dict);
+        knownCharacters.insert(in);
+        return;
+    }
+    
+    for (unsigned int i = 0; i < in.size(); i++)
+    {
+        string copyOfIn = in;
+        if (copyOfIn[i] != '-') continue;
+        copyOfIn[i] = floating[0];
+        computeKnownCharacters(copyOfIn, floating.substr(1), ret, dict, knownCharacters);
+    }
+}
+
+void computeUnknownCharacters(string in, set<string>& out, size_t indexToInsert, const set<string>& dict)
+{
+    if (indexToInsert >= in.size())
+    {
+        if (dict.find(in) != dict.end()) out.insert(in);
+        return;
+    }
+    if (in[indexToInsert] != '-')
+    {
+        computeUnknownCharacters(in, out, indexToInsert + 1, dict);
+        return;
+    }
+    
+    for (int i = 0; i < 26; i++)
+    {
+        char c = 'a' + i;
+        in[indexToInsert] = c;
+        computeUnknownCharacters(in, out, indexToInsert + 1, dict);
+    }
+}
